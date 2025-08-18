d41c43b27c382c45090d8b5f7c17810c5a5a8736
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-08-18 14:43:03 +0300

    Added Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Mulish, Playfair_Display } from "next/font/google";
 
 import { Cursor } from "@/components/cursor";
@@ -43,6 +44,7 @@ export default function RootLayout({
           {children}
           <Cursor />
         </CursorProvider>
+        <Analytics />
       </body>
     </html>
   );
